f0b0cc04d3d4da280a88aa2e222a5b31e7b75b55
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2022-03-07 12:27:52 -0800

    doing

--- App.js
+++ App.js
@@ -11,27 +11,85 @@ export default function App() {
   const ali = 23;
   return (
 
-    <View >
+    <View style={styles.container}>
+      <View style={styles.main1}>
+        <Text style={styles.textStyle}>Header</Text>
+      </View>
+      <View style={styles.main2}>
+        {/* <Text style={styles.textStyle}>Body</Text> */}
+        <View style={styles.boxes}>
+          <View style={styles.items}>
+          </View>
+          <View style={styles.items}>
+          </View>
+          <View style={styles.items}>
+          </View>
+          <View style={styles.items}>
+          </View>
+          <View style={styles.items}>
+          </View>
+          <View style={styles.items}>
+          </View>
+        </View>
+
+      </View>
+      <View style={styles.main3}>
+        <Text style={styles.textStyle}>Footer</Text>
+
+      </View>
       {/* <Text style={styles.textStyle}>Hello Wajahat Ahmed ye ma ho gullo</Text>
       <Text>my app.js {ali}</Text> */}
       {/* <CustomComponent /> */}
       {/* <FlatDemo /> */}
       {/* <Buttons /> */}
       {/* <Images /> */}
-      <NetflixCard />
+      {/* <NetflixCard /> */}
       {/* <StatusBar style="auto" /> */}
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
+    flex: 1,//by default whole screen lega
+    backgroundColor: 'green',
+    alignItems: 'center',
     justifyContent: 'center',
   },
   textStyle: {
-    color: 'red',
+    color: 'white',
+    fontSize: 24,
+    textAlign: 'center'
+  },
+
+  main1: {
+    backgroundColor: 'yellow',
+    width: "100%",
+    flex: 1
+  },
+  main2: {
+    backgroundColor: 'red',
+    width: "100%",
+    flex: 8
+  },
+  main3: {
+    backgroundColor: 'black',
+    width: "100%",
+    flex: 1
+  },
+  boxes: {
+    flex: 1,
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+    alignContent: 'center'
+  },
+  items: {
+    marginBottom: 10,
+    backgroundColor: 'white',
+    width: 100,
+    height: 100
   }
 });
