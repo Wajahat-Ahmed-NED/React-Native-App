5bbbcd66b9d4a3b17285db2475a0e6eb523746ef
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2022-03-08 11:48:01 -0800

    okay

--- src/config/Navigation.js
+++ src/config/Navigation.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from '../screens/Home';
 import About from '../screens/About';
 import Contact from '../screens/Contact';
+import { Image } from 'react-native-web';
 
 const Stack = createNativeStackNavigator()
 
@@ -11,7 +12,15 @@ export default function Navigation() {
         return (
                 <NavigationContainer>
                         <Stack.Navigator>
-                                <Stack.Screen name="Home" component={Home} options={{ title: 'Welcome' }} />
+                                <Stack.Screen name="Home" component={Home} options={{
+                                        title: 'Welcome',
+                                        // headerStyle: {
+                                        //         backgroundColor: 'orange'
+                                        // },
+                                        headerTitle: props => <Image style={{ height: 80, width: 80, resizeMode: 'contain' }} source={{ uri: "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/streams/2013/March/130326/1C6639340-google-logo.jpg" }} />
+                                }}
+
+                                />
                                 <Stack.Screen name="About" component={About} />
                                 <Stack.Screen name="Contact" component={Contact} />
                         </Stack.Navigator>
